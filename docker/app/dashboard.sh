74c59b8307b5bebaeedc9f09e913c6942d031328
Clement Sam <clementsam75@gmail.com>
2024-08-07 13:46:52 +0000

    Add a volume mount to persist binaries
    Change-Id: I1db746f7845c064f8b97853f743ea2d1a2785d11

--- docker/app/dashboard.sh
+++ docker/app/dashboard.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./storagenode dashboard --config-dir /app/config --identity-dir /app/identity $@
+/app/bin/storagenode dashboard --config-dir /app/config --identity-dir /app/identity $@
